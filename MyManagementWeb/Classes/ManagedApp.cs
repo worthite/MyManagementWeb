4bb37db6bcf85eff13e2874bd29b0d17434032eb
Tracy Worthington <worthington_t@hotmail.com>
2018-08-18 07:26:10 -0700

    1.2

---
+++ MyManagementWeb/Classes/ManagedApp.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+public class Properties
+{
+    public string managedResourceGroupId { get; set; }
+    public string applicationDefinitionId { get; set; }
+    public string publisherPackageId { get; set; }
+
+    public string provisioningState { get; set; }
+}
+
+public class Value
+{
+    public Properties properties { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+    public string type { get; set; }
+    public string kind { get; set; }
+    public string location { get; set; }
+}
+
+public class ManagedApps
+{
+    public List<Value> value { get; set; }
+}
